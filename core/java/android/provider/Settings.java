23a824acbca3b9abca013c9ca1767e61b4716e8b
Adnan Begovic <adnan@cyngn.com>
2013-11-04 00:58:57 +0000

    Implement linked volumes and add upgrade path.
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	media/java/android/media/AudioService.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1256,6 +1256,7 @@ public final class Settings {
             MOVED_TO_SECURE.add(Secure.WIFI_WATCHDOG_PING_COUNT);
             MOVED_TO_SECURE.add(Secure.WIFI_WATCHDOG_PING_DELAY_MS);
             MOVED_TO_SECURE.add(Secure.WIFI_WATCHDOG_PING_TIMEOUT_MS);
+            MOVED_TO_SECURE.add(Secure.VOLUME_LINK_NOTIFICATION);
 
             // At one time in System, then Global, but now back in Secure
             MOVED_TO_SECURE.add(Secure.INSTALL_NON_MARKET_APPS);
@@ -6410,6 +6411,13 @@ public final class Settings {
          * @hide
          */
         public static final String QS_USE_FOUR_COLUMNS = "qs_use_four_columns";
+
+        /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
          * Stored color matrix for LiveDisplay. This is used to allow co-existence with
