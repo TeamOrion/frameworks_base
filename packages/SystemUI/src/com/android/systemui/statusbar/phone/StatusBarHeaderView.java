a69434dc1d5bf925caabb121e12a1a7cc606ff5e
Alex Cruz <mazdarider23@gmail.com>
2015-05-25 05:14:13 +0000

    Fixed Task Manager bug described in post below
    https://plus.google.com/101691799763674764197/posts/Z7XFJSfsCYz
    
    Change-Id: If4f3f3c67c80cd8be3111ea3dbc3ac1f3b6d7618

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -403,8 +403,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         if (mTaskManagerButton != null) {
             mTaskManagerButton.setVisibility(mExpanded ? View.VISIBLE : View.GONE);
         }
-        mWeatherContainer.setVisibility(mExpanded && mShowWeather ? View.VISIBLE : View.GONE);
-        mQsDetailHeader.setVisibility(mExpanded && mShowingDetail? View.VISIBLE : View.INVISIBLE);
+        mQsDetailHeader.setVisibility(mExpanded && mShowingDetail ? View.VISIBLE : View.GONE);
         if (mSignalCluster != null) {
             updateSignalClusterDetachment();
         }
