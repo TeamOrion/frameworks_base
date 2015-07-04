45908db50b44b5c4fffe86b993c0eb0be3259f7d
LuK1337 <priv.luk@gmail.com>
2015-07-04 12:23:06 +0200

    ActivityStack: Delay Binder Explicit GC during application launch
    Change-Id: I5deefeae474643df1522d4214b49d7be2690e6bf

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2725,6 +2725,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
 
     ActivityRecord findTaskLocked(ActivityRecord r) {
         if (DEBUG_TASKS) Slog.d(TAG, "Looking for task of " + r);
+        /* Delay Binder Explicit GC during application launch */
+        BinderInternal.modifyDelayedGcParams();
         for (int displayNdx = mActivityDisplays.size() - 1; displayNdx >= 0; --displayNdx) {
             final ArrayList<ActivityStack> stacks = mActivityDisplays.valueAt(displayNdx).mStacks;
             for (int stackNdx = stacks.size() - 1; stackNdx >= 0; --stackNdx) {
