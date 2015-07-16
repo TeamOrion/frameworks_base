d78a771918071ea8f8a1449c958a38cddf2c5921
Jmz <jmztaylor@gmail.com>
2015-07-16 17:21:42 -0400

    Force Expanded Notifications (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4018,6 +4018,13 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_HIDE_ARROW = "network_traffic_hide_arrow";
+ 
+         /**
+         * Height of the gesture anywhere trigger.
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
