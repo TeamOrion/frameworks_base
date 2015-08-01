eae48af85ed8fac8e6c00eaa5515a75f6cd5bcd6
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-08-01 02:31:30 -0700

    Recents: Launch as Floating app.
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Change-Id: I591a566c78f14dae14bbd967116138013876892a
    
    Conflicts:
    	packages/SystemUI/res/values/custom_strings.xml
    
    Conflicts:
    	packages/SystemUI/res/values/slimremix_strings.xml

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -63,6 +63,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
         public void onTaskViewClicked(TaskStackView stackView, TaskView tv, TaskStack stack, Task t,
                                       boolean lockToTask);
         public void onTaskViewAppInfoClicked(Task t);
+        public void onTaskFloatClicked(Task t);
         public void onTaskViewDismissed(Task t);
         public void onAllTaskViewsDismissed();
         public void onTaskStackFilterTriggered();
@@ -1122,6 +1123,13 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
     }
 
     @Override
+    public void onTaskFloatClicked(TaskView tv) {
+        if (mCb != null) {
+            mCb.onTaskFloatClicked(tv.getTask());
+        }
+    }
+
+    @Override
     public void onTaskViewLongClicked(final TaskView tv) {
         final PopupMenu popup = new PopupMenu(getContext(), tv.mHeaderView.mApplicationIcon);
         mPopup = popup;
@@ -1155,6 +1163,9 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         break;
                     case R.id.recent_inspect_item:
                         onTaskViewAppInfoClicked(tv);
+                        break;
+                    case R.id.recent_float_mode:
+                        onTaskFloatClicked(tv);
                         break;
                     case R.id.recent_force_stop:
                     {
