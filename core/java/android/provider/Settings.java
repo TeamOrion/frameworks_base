4ef0d4a9028bba4a8235523cdba2b6116b659520
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-08-16 15:13:00 +0200

    Add option for left clock style / position date left-right of clock [1/2]
    This adds :
    - clock positioned on the left
    - option to position date left or right of clock
    
    PS2 : add msim support
    PS4 : fix SystemUI FC when using small font am/pm AND small font date
    
    Change-Id: Ib14433baec99dfd7f68ce62294e710a825c482bb
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3606,13 +3606,21 @@ public final class Settings {
 
         /**
          * Style of clock
-         * 0 - Hide Clock
-         * 1 - Right Clock
-         * 2 - Center Clock
+         * 0 - Right Clock
+         * 1 - Center Clock
+         * 2 - Left Clock
          * @hide
          */
         public static final String STATUSBAR_CLOCK_STYLE = "statusbar_clock_style";
 
+         /**
+          * Position of date
+          * 0 - Left of clock
+          * 1 - Right of clock
+          * @hide
+          */
+         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
+         
         /**
          * Setting for clock color
          * @hide
