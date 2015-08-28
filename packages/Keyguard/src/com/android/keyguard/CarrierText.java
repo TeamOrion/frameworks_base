503e6610333651855218dea8685ff00c74749907
LorDClockaN <davor@losinj.com>
2015-08-27 22:03:21 -0700

    FWB: Hide carrier on lock screen (1/2)
    PS2:
    Simplify it a bit
    
    Change-Id: I2384392606547349d6252c5461f6e6727dcd7e08
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/CarrierText.java
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/CarrierText.java

--- packages/Keyguard/src/com/android/keyguard/CarrierText.java
+++ packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -16,6 +16,7 @@
 
 package com.android.keyguard;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -23,6 +24,7 @@ import android.content.res.TypedArray;
 import android.net.ConnectivityManager;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.method.SingleLineTransformationMethod;
 import android.util.AttributeSet;
@@ -171,7 +173,12 @@ public class CarrierText extends TextView {
                         getContext().getText(R.string.keyguard_missing_sim_message_short), text);
             }
         }
-        setText(displayText);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCK_SCREEN_HIDE_CARRIER, 0) == 0) {
+            setText(displayText);
+        } else {
+            setText("");
+        }
     }
 
     @Override
