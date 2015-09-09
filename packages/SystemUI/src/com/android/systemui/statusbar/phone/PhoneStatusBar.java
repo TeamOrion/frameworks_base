9daea78094e2c3854937a514ed90891423a8b429
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-09 14:30:23 -0500

    SystemUI: reset to QS panel if task manager was visible last
    *also don't allow task manager to show on lock screen

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3932,6 +3932,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mCurrentUserId);
     }
 
+    public void resetQsPanelVisibility() {
+        showTaskList = showTaskList;
+        if (showTaskList) {
+            mQSPanel.setVisibility(View.VISIBLE);
+            mTaskManagerPanel.setVisibility(View.GONE);
+            showTaskList = false;
+        }
+    }
+
     private void setHeadsUpVisibility(boolean vis) {
         if (DEBUG) Log.v(TAG, (vis ? "showing" : "hiding") + " heads up window");
         EventLog.writeEvent(EventLogTags.SYSUI_HEADS_UP_STATUS,
