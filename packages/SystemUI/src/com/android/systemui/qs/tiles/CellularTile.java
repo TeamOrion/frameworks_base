62fcb075f4dc4e70ca3cb8948f13ae90466b34db
Yeshwanth <yeshwanthvshenoy@gmail.com>
2015-09-10 17:01:00 +0000

    Single click Toggle, Long click Summary

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -89,18 +89,18 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     @Override
     protected void handleClick() {
         if (mDataController.isMobileDataSupported()) {
-            showDetail(true);
+            mDataController.setMobileDataEnabled(!mDataController.isMobileDataEnabled());
         } else {
-            mHost.startSettingsActivity(DATA_USAGE_SETTINGS);
+            mDataController.setMobileDataEnabled(mDataController.isMobileDataEnabled());
         }
     }
 
     @Override
     protected void handleLongClick() {
         if (mTelephonyManager.getDefault().getPhoneCount() > 1) {
-            mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS_MSIM);
+            showDetail(true);
         } else {
-            mHost.startSettingsActivity(MOBILE_NETWORK_SETTINGS);
+            showDetail(true);
         }
     }
 
