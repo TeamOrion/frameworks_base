8305a43931a7a256d7e8e04696087fa706a162f4
Alex Cruz <mazdarider23@gmail.com>
2016-02-12 21:44:17 -0500

    Add IME selector QS Tile
    See image - http://i.imgur.com/83vHFI0.png
    
    Change-Id: I48fd585901cecd24b0ba94409e69b6a5cda907b1
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/purenexus_strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
@@ -304,6 +305,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(this);
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("compass")) return new CompassTile(this);
+        else if (tileSpec.equals("ime")) return new ImeTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -213,6 +213,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("adb_network")) return R.string.quick_settings_adb_network;
         else if (spec.equals("music")) return R.string.quick_settings_music_label;
         else if (spec.equals("compass")) return R.string.quick_settings_compass_label;
+        else if (spec.equals("ime")) return R.string.quick_settings_ime_label;
         return 0;
     }
 
@@ -466,6 +467,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("adb_network")) return R.drawable.ic_qs_network_adb_on;
             else if (mSpec.equals("music")) return R.drawable.ic_qs_media_play;
             else if (mSpec.equals("compass")) return R.drawable.ic_qs_compass_on;
+            else if (mSpec.equals("ime")) return R.drawable.ic_qs_ime;
             return R.drawable.android;
         }
 
