0768d2b9ea864af4dd6b63bed6cddc28b7fbfae2
sarthakspiker <spikerstriker@gmail.com>
2016-03-12 11:27:28 +0530

    gesturefixing

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1471,7 +1471,7 @@ public final class PowerManagerService extends SystemService
     private boolean shouldWakeUpWhenPluggedOrUnpluggedLocked(
             boolean wasPowered, int oldPlugType, boolean dockedOnWirelessCharger) {
         // Don't wake when powered unless configured to do so.
-        if (mWakeUpWhenPluggedOrUnpluggedConfig == 0 ) {
+        if (mWakeUpWhenPluggedOrUnpluggedSetting == 0 ) {
             return false;
         }
 
