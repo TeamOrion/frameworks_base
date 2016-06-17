2a981ddc14b870a817739e5d4357d2a90d9fc09a
Sravanthi Palakonda <srapal@codeaurora.org>
2016-06-17 19:30:02 +0530

    wifi: Enhance WiFi Configuration to also include sim number.
    For EAP SIM/AKA/AKA` authentication to happen on a specific SIM , the
    SIM number has to be also a part of Wi-Fi configuration . This commit
    ensures the same.
    
    Change-Id: I3239c534bd8d221dc0af0174304427491a1d586b
    CRs-Fixed: 1030715
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -69,6 +69,8 @@ public class WifiConfiguration implements Parcelable {
 
     /** {@hide} */
     private String mPasspointManagementObjectTree;
+    /** {@hide} */
+    public static final String SIMNumVarName = "sim_num";
 
     /**
      * Recognized key management schemes.
@@ -445,6 +447,12 @@ public class WifiConfiguration implements Parcelable {
      */
     @SystemApi
     public String lastUpdateName;
+
+    /**
+     * @hide
+     * sim number selected
+     */
+    public int SIMNum;
 
     /**
      * @hide
@@ -1351,6 +1359,7 @@ public class WifiConfiguration implements Parcelable {
         creatorUid = -1;
         shared = true;
         dtimInterval = 0;
+        SIMNum = 0;
     }
 
     /**
@@ -1510,6 +1519,10 @@ public class WifiConfiguration implements Parcelable {
         }
         sbuf.append('\n').append(" PSK: ");
         if (this.preSharedKey != null) {
+            sbuf.append('*');
+        }
+        sbuf.append('\n').append(" sim_num ");
+        if (this.SIMNum > 0 ) {
             sbuf.append('*');
         }
         sbuf.append("\nEnterprise config:\n");
@@ -1908,6 +1921,7 @@ public class WifiConfiguration implements Parcelable {
             creationTime = source.creationTime;
             updateTime = source.updateTime;
             shared = source.shared;
+            SIMNum = source.SIMNum;
         }
     }
 
@@ -1978,6 +1992,7 @@ public class WifiConfiguration implements Parcelable {
         dest.writeInt(noInternetAccessExpected ? 1 : 0);
         dest.writeInt(shared ? 1 : 0);
         dest.writeString(mPasspointManagementObjectTree);
+        dest.writeInt(SIMNum);
     }
 
     /** Implement the Parcelable interface {@hide} */
@@ -2049,6 +2064,7 @@ public class WifiConfiguration implements Parcelable {
                 config.noInternetAccessExpected = in.readInt() != 0;
                 config.shared = in.readInt() != 0;
                 config.mPasspointManagementObjectTree = in.readString();
+                config.SIMNum = in.readInt();
                 return config;
             }
 
