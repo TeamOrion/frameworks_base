f5f05e848920230718e2fa2a289da1ad77c19714
beanstown106 <nbenis106@gmail.com>
2017-01-16 22:27:17 -0500

    One handed mode toggle (1/2)
    Change-Id: Ifcf7c2cd0162724a610a2e9620eab14e9ea09b40
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6670,6 +6670,12 @@ public final class Settings {
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
          /**
+         * One Handed mode toggle
+         * @hide
+         */
+        public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
+
+        /**
          * Status bar battery %
          * 0: Hide the battery percentage
          * 1: Display the battery percentage inside the icon
